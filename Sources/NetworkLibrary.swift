c954a04e88c73aab6acb774aa3238a6b1156a2d7
Alex Antonyuk <alex@antonyuk.me>
2017-11-07 15:53:52 +0200

    1.0.1

--- Sources/NetworkLibrary.swift
+++ Sources/NetworkLibrary.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 final public class NetworkLibrary {
-	public func get(url: URL, _ callback: @escaping (String) -> Void) {
+	public class func get(url: URL, _ callback: @escaping (String) -> Void) {
 		URLSession.shared.dataTask(with: url) { (data, _, _) in
 			if let data = data, let str = String(bytes: data, encoding: .utf8) {
 				callback(str)
